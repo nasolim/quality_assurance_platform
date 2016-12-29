1889b186ae0fa6ab72252519b283972fbeae097c
Milo Sanu <camilosanu@gmail.com>
2016-12-29 16:02:16 -0500

    First Commit

---
+++ app/Survey.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Survey extends Model
+{
+    // A survey has many questions
+    public function build_survey(){
+        
+    }
+}
---
+++ database/migrations/2016_12_29_202652_create_questions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('report_id');
+            $table->integer('question_number');
+            $table->integer('is_critical');
+            $table->string('question_type');
+            $table->string('question_text');
+            $table->string('answer_type');
+            $table->float('point_assignment');
+            $table->string('created_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
